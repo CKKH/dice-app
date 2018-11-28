6fa0557918635f4f7c476cecec07dfba0e655f20
CKKH <chriskkhassan@gmail.com>
2018-11-28 12:13:53 +0000

    .roll returns random integer between 1-6

--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -1,5 +1,11 @@
 require 'dice'
 
 describe Dice do
-  it { is_expected.to respond_to(:roll)}
-end
+
+  it { is_expected.to respond_to(:roll) }
+
+  it 'returns an integer in the range 1-6' do
+       dice = Dice.new
+       expect(dice.roll).to be_between(1, 6)
+     end
+  end
